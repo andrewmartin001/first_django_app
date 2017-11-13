1820a0d64572008e1f98f08c3150347e7957663f
andrewmartin <andrew.d.martin@ntlworld.com>
2017-11-14 11:34:25 +1300

    changed range slider to hourly

--- mysite/weather_db/populate_db.py
+++ mysite/weather_db/populate_db.py
@@ -29,21 +29,9 @@ def plot_slider(x_data, y_data, x_title, y_title, filename):
             rangeselector=dict(
                 buttons=list([
                     dict(count=1,
-                         label='1m',
-                         step='month',
+                         label='1h',
+                         step='hour',
                          stepmode='backward'),
-                    dict(count=6,
-                         label='6m',
-                         step='month',
-                         stepmode='backward'),
-                    dict(count=1,
-                        label='YTD',
-                        step='year',
-                        stepmode='todate'),
-                    dict(count=1,
-                        label='1y',
-                        step='year',
-                        stepmode='backward'),
                     dict(step='all')
                 ])
             ),
